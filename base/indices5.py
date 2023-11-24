878ad639bff9ac458c149c4815fb29684c91dcd7
Gennadiy <renardes@gmail.com>
2023-11-24 09:52:35 +0500

    примеры использования индексации

---
+++ base/indices5.py
@@ -0,0 +1,5 @@
+dir_name = '.git'
+
+if dir_name[0] == '.':
+    print(f'{dir_name} — скрытый каталог')
+
---
+++ base/indices6.py
@@ -0,0 +1,11 @@
+number = input('введите число > ')
+
+digit1 = int(number[0])
+digit2 = int(number[1])
+digit3 = int(number[2])
+
+print(
+    digit1 + digit2 + digit3, 
+    digit1 * digit2 * digit3,
+    sep='\n'
+)
