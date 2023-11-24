c700aa98d09ea3dc4698ebad97ad8b82335bc6a5
Gennadiy <renardes@gmail.com>
2023-11-24 09:50:47 +0500

    особенности последовательностей str

---
+++ base/indices1.py
@@ -0,0 +1,28 @@
+text = 'python is cool'
+
+# >>> text
+# 'python is cool'
+# >>> type(text)
+# <class 'str'>
+
+# >>> text[0]
+# 'p'
+# >>> type(text[0])
+# <class 'str'>
+
+char = text[0]
+
+# >>> char
+# 'p'
+# >>> char[0]
+# 'p'
+
+empty_str = ''
+
+# >>> type(empty_str)
+# <class 'str'>
+
+# >>> empty_str[0]
+# ...
+# IndexError: string index out of range
+
