ba26d09033c7f697dda52d7dae79eb623f457bb5
Gennadiy <renardes@gmail.com>
2023-12-29 19:03:37 +0500

    абсолютные и относительные пути в разных ОС и в виде литералов Python

---
+++ base/paths1.py
@@ -0,0 +1,38 @@
+# абсолютный путь Windows
+# C:\Users\Antoshka\Scripts\project1\main.py
+
+abs_path1 = 'C:\\Users\\Antoshka\\Scripts\\project1\\main.py'
+
+# cwd - current working directory (текущий рабочий каталог)
+# C:\Users\Antoshka\Scripts
+
+abs_path2 = r'C:\Users\Antoshka\Scripts'
+
+# относительный путь Windows
+# project1\main.py
+
+rel_path1 = 'project1\\main.py'
+
+# cwd - current working directory (текущий рабочий каталог)
+# C:\Users\Antoshka
+
+# относительный путь Windows
+# Scripts\project1\main.py
+
+rel_path2 = r'project1\main.py'
+
+
+# абсолютный путь Unix
+# /home/antoshka/scripts/project1/main.py
+
+# cwd - current working directory (текущий рабочий каталог)
+# /home/antoshka/scripts
+
+# относительный путь Unix
+# project1/main.py
+
+# cwd - current working directory (текущий рабочий каталог)
+# /home/antoshka
+
+# относительный путь Unix
+# scripts/project1/main.py
