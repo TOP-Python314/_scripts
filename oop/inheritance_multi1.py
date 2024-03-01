46b208e2da246ff139c2974aed73448672ea0f0d
Gennadiy <renardes@gmail.com>
2024-03-01 10:38:24 +0500

    порядок наследования от нескольких классов

---
+++ oop/inheritance_multi1.py
@@ -0,0 +1,27 @@
+class A:
+    attr = 'атрибут класса A'
+
+
+class B:
+    attr = 'атрибут класса B'
+
+
+class C(A, B):
+    pass
+
+
+class D(B, A):
+    pass
+
+
+# >>> C.attr
+# 'атрибут класса A'
+# >>>
+# >>> D.attr
+# 'атрибут класса B'
+
+# >>> C.__mro__
+# (<class '__main__.C'>, <class '__main__.A'>, <class '__main__.B'>, <class 'object'>)
+# >>>
+# >>> D.__mro__
+# (<class '__main__.D'>, <class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
