de558344cdbec6f526e1276d03fb68632c7fc158
Gennadiy <renardes@gmail.com>
2024-03-23 11:12:24 +0500

    принцип подстановки Лисков: встроенная модель Python

---
+++ oop/solid_lsp_1.py
@@ -0,0 +1,23 @@
+"""Liskov Substitution Principle — Принцип Подстановки Лисков"""
+
+# >>> int
+# <class 'int'>
+# >>>
+# >>> int.__mro__
+# (<class 'int'>, <class 'object'>)
+
+# >>> bool
+# <class 'bool'>
+# >>>
+# >>> bool.__mro__
+# (<class 'bool'>, <class 'int'>, <class 'object'>)
+
+# >>> numbers = [10, 20, 30, 40]
+# >>>
+# >>> numbers[1]
+# 20
+# >>> numbers[True]
+# 20
+
+# >>> True + True + True
+# 3
